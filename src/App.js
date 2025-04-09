25fdf84bcc989c363dbc9b36ce776b360305dbc1
Tamima <75810859+TairatulJannat@users.noreply.github.com>
2025-04-09 12:49:09 +0600

    Initial commit2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import Navbar from './components/Navbar';
@@ -56,7 +56,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router basename='/portfolio'>
+      <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
